97bcc10dbb783241fe189cbb178ccf00e24b6416
Umang Yadav <umang.work82@gmail.com>
2025-08-10 19:03:37 +0530

    config: add spotless [I-1]

--- src/main/java/com/opensource/mock_server/MockServerApplication.java
+++ src/main/java/com/opensource/mock_server/MockServerApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MockServerApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MockServerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(MockServerApplication.class, args);
+  }
 }
